9dc387671577e5f5f1dd461e90e81b538b0aa57d
ryanxjacques <58966393+ryanxjacques@users.noreply.github.com>
2023-09-08 16:08:50 -0700

    Added resize events in JS to auotmatically collapse buttons into menu, and close the menu when resized back into landscape (default) viewport.

--- src/menu.js
+++ src/menu.js
@@ -2,40 +2,72 @@
  * Handle menu clicks and displays
  */
 
-const menu = document.getElementById('menu-icon');
+const menuIcon = document.getElementById('menu-icon');
+const closeMenuIcon = document.getElementById('close-menu-icon');
 
-const closeMenu = document.getElementById('close-menu-icon');
+const dropdownIcons = ["github-icon", "linkedin-icon", "email-icon", "dark-light-mode-small-media-icon", "close-menu-icon"];
 
-// One liner function:
-// Creds: https://stackoverflow.com/users/1602301/chickens
-const addCSS = (css) => document.head.appendChild(document.createElement("style")).innerHTML = css;
+// Function to open and close menu
+const toggleMenu = () => {
 
+    // Menu is closed 
+    if (menuIcon.dataset.status === "closed") {
+        
+        // Hide the menu item
+        menuIcon.style.display = "none";
 
-const hideMenuCSS = `
-    #menu-icon {
-        display: none;
+        // Drop down the github, linkedin, and mail icons in the same place
+        for (const icon of dropdownIcons) {
+            document.getElementById(icon).style.display = "block";
+        }
+        menuIcon.dataset.status = "open";
     }
-`;
-
-const dropdownIcons = ["github-icon", "linkedin-icon", "email-icon", "dark-light-mode-small-media-icon", "close-menu-icon"]
-
-menu.addEventListener('click', (e) => {
-
-    // Hide the menu item
-    menu.style.display = "none";
-
-    // Drop down the github, linkedin, and mail icons in the same place
-    for (const icon of dropdownIcons) {
-        document.getElementById(icon).style.display = "block";
-    }
-    // Contain them in a ghost white background??
-});
-
-closeMenu.addEventListener('click', (e) => {
-    // Remove the github, linkedin, and mail icons
+    else {
+        // Remove the github, linkedin, and mail icons
         for (const icon of dropdownIcons) {
             document.getElementById(icon).style.display = "none";
         }
-    // Show the menu item
-    menu.style.display = "block";
+        // Show the menu item
+        menuIcon.style.display = "block";
+
+        menuIcon.dataset.status = "closed";
+    }
+}
+
+menuIcon.addEventListener('click', () => {
+    toggleMenu();
+});
+
+closeMenuIcon.addEventListener('click', () => {
+    toggleMenu();
+});
+
+/* Replace the buttons back to default landscape mode and close the menu.*/
+const landscapeResize = () => {
+    if (menuIcon.dataset.status === "open") {
+        // Close menu
+        toggleMenu();
+    }
+    // Reset css only if menu is visible
+    if (menuIcon.style.display === "block") {
+        document.getElementById(dropdownIcons[0]).style.display = "block"; // Github icon 
+        document.getElementById(dropdownIcons[1]).style.display = "block"; // Linkedin icon 
+        document.getElementById(dropdownIcons[2]).style.display = "block"; // Email icon
+        menuIcon.style.display = "none"; // menu
+    }
+}
+
+/* Collapse buttons into menu */
+const portraitResize = () => {
+    if (menuIcon.dataset.status === "closed") {
+        for (const icon of dropdownIcons) {
+            document.getElementById(icon).style.display = "none";
+            menuIcon.style.display = "block";
+        }
+    }
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 750) { landscapeResize(); }
+    else { portraitResize(); }
 });
