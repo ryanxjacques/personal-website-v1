836734361f61a769c2600cc23d28c2a6f2bf430e
ryanxjacques <58966393+ryanxjacques@users.noreply.github.com>
2023-09-10 19:37:27 -0700

    Finished menu transition/animation.

--- src/menu.js
+++ src/menu.js
@@ -1,37 +1,122 @@
 /**
- * Handle menu clicks and displays
+ * Menu activity
  */
 
 const menuIcon = document.getElementById('menu-icon');
+
+// Dropdown elements
+const githubIcon = document.getElementById('github-icon')
+const linkedinIcon = document.getElementById('linkedin-icon')
+const emailIcon = document.getElementById('email-icon')
+const darkLightModeSmallMediaIcon = document.getElementById('dark-light-mode-small-media-icon')
 const closeMenuIcon = document.getElementById('close-menu-icon');
 
-const dropdownIcons = ["github-icon", "linkedin-icon", "email-icon", "dark-light-mode-small-media-icon", "close-menu-icon"];
+const dropdownIcons = [githubIcon, linkedinIcon, emailIcon, darkLightModeSmallMediaIcon, closeMenuIcon];
+
+/**
+ * Toggling Menu
+ */
+
+
+const openMenuTransition = () => {
+    // Transition duration
+    let transitionDuration = 400;
+    // Hide the menu item
+    menuIcon.style.display = "none";
+
+    // Drop down the github, linkedin, and mail icons in the same place
+    for (const icon of dropdownIcons) {
+
+        // Set initial opacity (e.g., fully transparent)
+        icon.style.opacity = "0";
+        icon.style.display = "block";
+
+        // Apply a CSS transition to gradually change opacity
+        icon.style.transition = `opacity ${transitionDuration}ms cubic-bezier(0.25, 0.1, 0.25, 1)`;
+
+        // Trigger a reflow (necessary for the transition to take effect)
+        icon.offsetWidth;
+
+        // Set the opacity to fully opaque
+        icon.style.opacity = "1";
+
+        transitionDuration += 400;
+    }
+}
+
+const showMenuTransition = () => {
+    menuIcon.style.display = "block";
+    menuIcon.style.opacity = "0";
+
+    // Apply a CSS transition to gradually change opacity
+    menuIcon.style.transition = "opacity 0.5s cubic-bezier(0.25, 0.1, 0.25, 1)";
+
+    // Trigger a reflow (necessary for the transition to take effect)
+    menuIcon.offsetWidth;
+
+    // Set the opacity to fully opaque
+    menuIcon.style.opacity = "1";
+}
+
+
+
+const closeMenuTransition = () => {
+    // Hide the menu item
+    menuIcon.style.display = "none";
+
+    // Drop down the github, linkedin, and mail icons in the same place
+    const reverseDropdownIcons = [...dropdownIcons].reverse();
+
+    // Calculate the total animation duration
+    let totalTransitionDuration = 400;
+
+    for (const icon of reverseDropdownIcons) {
+        // Apply a CSS transition to gradually change opacity
+        icon.style.transition = `opacity ${totalTransitionDuration}ms cubic-bezier(0.25, 0.1, 0.25, 1)`;
+
+        // Trigger a reflow (necessary for the transition to take effect)
+        icon.offsetWidth;
+
+        // Set the opacity to fully transparent
+        icon.style.opacity = "0";
+
+        totalTransitionDuration += 400;
+    }
+
+    // After the total animation duration, set up the transition finisher
+    setTimeout(() => {
+        // Hide all the icons and reset opacity
+        for (const icon of dropdownIcons) {
+            icon.style.display = "none";
+            icon.style.opacity = "1";
+        }
+
+        // Show the menu item
+        showMenuTransition();
+        // Set status
+    }, totalTransitionDuration - 1000); // subtract 400 for starting duration
+}
+
 
 // Function to open and close menu
 const toggleMenu = () => {
 
     // Menu is closed 
-    if (menuIcon.dataset.status === "closed") {
-        
-        // Hide the menu item
-        menuIcon.style.display = "none";
-
-        // Drop down the github, linkedin, and mail icons in the same place
-        for (const icon of dropdownIcons) {
-            document.getElementById(icon).style.display = "block";
-        }
+    if (menuIcon.dataset.status === "closed") {    
+        //Animate icon dropdown
+        openMenuTransition();   
+        // Swap status
         menuIcon.dataset.status = "open";
     }
+
     else {
         // Remove the github, linkedin, and mail icons
-        for (const icon of dropdownIcons) {
-            document.getElementById(icon).style.display = "none";
-        }
-        // Show the menu item
-        menuIcon.style.display = "block";
-
+        closeMenuTransition();
+        // Swap status
         menuIcon.dataset.status = "closed";
+        
     }
+
 }
 
 menuIcon.addEventListener('click', () => {
@@ -42,28 +127,38 @@ closeMenuIcon.addEventListener('click', () => {
     toggleMenu();
 });
 
+
+/**
+ * Resize windows events
+ */
+
+
+
 /* Replace the buttons back to default landscape mode and close the menu.*/
 const landscapeResize = () => {
     if (menuIcon.dataset.status === "open") {
+        
         // Close menu
-        toggleMenu();
+        darkLightModeSmallMediaIcon.style.display = "none"; // Hide light-dark-mode icon
+        closeMenuIcon.style.display = "none"; // Hide close menu icon
+        menuIcon.dataset.status = "closed";
     }
     // Reset css only if menu is visible
     if (menuIcon.style.display === "block") {
-        document.getElementById(dropdownIcons[0]).style.display = "block"; // Github icon 
-        document.getElementById(dropdownIcons[1]).style.display = "block"; // Linkedin icon 
-        document.getElementById(dropdownIcons[2]).style.display = "block"; // Email icon
-        menuIcon.style.display = "none"; // menu
-    }
+        githubIcon.style.display = "block";
+        linkedinIcon.style.display = "block";
+        emailIcon.style.display = "block"; 
+        menuIcon.style.display = "none"; 
+    }   
 }
 
 /* Collapse buttons into menu */
 const portraitResize = () => {
     if (menuIcon.dataset.status === "closed") {
         for (const icon of dropdownIcons) {
-            document.getElementById(icon).style.display = "none";
-            menuIcon.style.display = "block";
+            icon.style.display = "none";
         }
+        menuIcon.style.display = "block";
     }
 }
 
@@ -71,3 +166,4 @@ window.addEventListener('resize', () => {
     if (window.innerWidth > 750) { landscapeResize(); }
     else { portraitResize(); }
 });
+
